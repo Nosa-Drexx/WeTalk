c689b99e19e8daab3a7f91576c549e47766407f9
Nosa-Drexx <nosakhare.egharevba@physci.uniben.edu>
2023-04-20 23:41:05 +0100

    server initial stage

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,28 @@
-const app = require("express");
+import { Application, Request, Response } from "express";
+
+const express = require("express");
 const http = require("http");
 const cors = require("cors");
+const socketIO = require("socket.io");
 
-const io = require("socket.io");
-
+const app: Application = express();
 const server = http.createServer(app);
+
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+
+const PORT: number | string = process.env.port || 5000;
+
+app.get("/", (req: Request, res: Response): void => {
+  res.send("Server is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
